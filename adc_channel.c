7636016e790fad29ac7d301f3174a8e5a177d984
Mario Hideki Murata <106937976+muratahideki@users.noreply.github.com>
2025-09-20 10:40:48 -0300

    Update adc_channel.c

--- adc_channel.c
+++ adc_channel.c
@@ -1,22 +1,17 @@
-#include "driver/mcpwm.h"
+#include <array>
 #include "driver/adc.h"
-#include "esp_timer.h"
 #include "esp_log.h"
 
-// sugestão: usar só ADC1
-
-
-// --- Configuração ADC ---
-#define ADC1 ADC1_CHANNEL_3   // GPIO4 (exemplo, fase flutuante)
-#define ADC2 ADC1_CHANNEL_4   // GPIO5 (exemplo, fase flutuante)
-#define ADC3 ADC2_CHANNEL_6   // GPIO17 (exemplo, fase flutuante)
-#define ADC4 ADC2_CHANNEL_7   // GPIO18 (exemplo, fase flutuante)
-#define ADC5 ADC2_CHANNEL_8   // GPIO19 (exemplo, fase flutuante)
-#define ADC6 ADC2_CHANNEL_9   // GPIO20 (exemplo, fase flutuante)
-
-
 #define ADC_WIDTH   ADC_WIDTH_BIT_12
-#define ADC_ATTEN   ADC_ATTEN_DB_11  // até ~3.6V
+#define ADC_ATTEN   ADC_ATTEN_DB_11  // até ~3.6 V
+
+#define PHASE_A ADC1_CHANNEL_3
+#define PHASE_B ADC1_CHANNEL_4
+#define PHASE_C ADC1_CHANNEL_5
+#define PHASE_D ADC1_CHANNEL_6
+#define PHASE_E ADC1_CHANNEL_7
+#define PHASE_F ADC1_CHANNEL_0 
+
 
 int steps_adc[6] { 1,2,3,4,5,6 }; // verificar se é int msm essa função
 
@@ -24,43 +19,58 @@ int vbus = 12;
 int vbus_half = vbus / 2;
 
 std::array<int,6> read_ADC() {
-  return {adc1_get_raw(ADC1),adc1_get_raw(ADC2),adc2_get_raw(ADC3),adc2_get_raw(ADC4),adc2_get_raw(ADC5),adc2_get_raw(ADC6)
+    return {
+        adc1_get_raw(PHASE_A),
+        adc1_get_raw(PHASE_B),
+        adc1_get_raw(PHASE_C),
+        adc1_get_raw(PHASE_D),
+        adc1_get_raw(PHASE_E),
+        adc1_get_raw(PHASE_F)
     };
+}
     
-void comparator(int vbus_half, int steps_adc){
+int comparator(int vbus_half, int steps_adc){
   while(true) {
     auto adc_latest = read_ADC();
     // return step = 4 ou 1
-    if (adc[0,1] < 100) {
-      auto adc[0,1] = read_ADC()
+    if (adc[0] < 100 && adc[1] < 100) {
+      auto adc_a = read_ADC();
       //cruzando para cima 
-      if ( adc_latest[0,1] < vbus_half && adc[0,1] >= vbus_half){
-        return steps_adc[1] };
+      if ( adc_latest[0] < vbus_half && adc_a[0] >= vbus_half ){
+        return steps_adc[1] 
+      };
+      adc_latest[0] = adc_a[0];
       //cruzando para baixo 
-      elif ( adc_latest[0,1] >= vbus_half && adc[0,1] < vbus_half){
-        return steps_adc[4]};     
+      elif ( adc_latest[1] >= vbus_half && adc_a[1] < vbus_half){
+        return steps_adc[4]
+      };     
+      adc_latest[1] = adc_a[1];
+    };
+
+    if (adc[2] < 100 && adc[3] < 100) {
+      auto adc_b = read_ADC()
+        if ( adc_latest[2] < vbus_half && adc_b[2] >= vbus_half){
+          return steps_adc[3] 
         };
-        adc_latest[0,1] = adc[0,1];
-  
-    if (adc[2,3] < 100) {
-      auto adc[2,3] = read_ADC()
-        if ( adc_latest[2,3] < vbus_half && adc[2,3] >= vbus_half){
-          return steps_adc[3] };
-        //cruzando para baixo 
-        elif ( adc_latest[2,3] >= vbus_half && adc[2,3] < vbus_half){
+        adc_latest[2] = adc_b[2];
+
+        elif ( adc_latest[3] >= vbus_half && adc_b[3] < vbus_half){
           return steps_adc[6]};
         };
-        adc_latest[2,3] = adc[2,3];
+        adc_latest[3] = adc_b[3];
+    };
 
-    if (adc[4,5] < 100) {
-      auto adc[4,5] = read_ADC()
-        if ( adc_latest[4,5] < vbus_half && adc[4,5] >= vbus_half){
-          return steps_adc[5] };
-        //cruzando para baixo 
-        elif ( adc_latest[4,5] >= vbus_half && adc[4,5] < vbus_half){
-          return steps_adc[2]};
+    if (adc[4] < 100 && adc[5] < 100 ) {
+      auto adc = read_ADC()
+        if ( adc_latest[4] < vbus_half && adc[4] >= vbus_half){
+          return steps_adc[5] 
         };
-        adc_latest[4,5] = adc[4,5];
+        adc_latest[4] = adc[4];
+        elif ( adc_latest[5] >= vbus_half && adc[5] < vbus_half){
+          return steps_adc[2]
+        };
+        adc_latest[5] = adc[5];
+    };
   };
 
 // --- Polling para detectar Zero Crossing ---
@@ -69,6 +79,7 @@ int wait_for_ZC(int vbus_half){
   while (true) { 
     void comparator() 
   };
+};
 
 
 
